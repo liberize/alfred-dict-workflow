e76ebac7f5de3dfeb3db00f1441813ac0dfb4819
liberize <liberize@gmail.com>
2014-10-14 14:27:14 +0800

    fix bug

--- Dict - Lookup Word/actions.py
+++ Dict - Lookup Word/actions.py
@@ -5,13 +5,23 @@ import sys
 import os
 import re
 import cndict
+import plistlib
 
 if len(sys.argv) != 2:
     sys.exit(1)
 
 
+def get_keyword():
+    plist = plistlib.readPlist(os.path.abspath('./info.plist'))
+    for item in plist['objects']:
+        if 'keyword' in item['config']:
+            return item['config']['keyword']
+    return None
+
+
 def restore(arg):
     os.system("""osascript -e 'tell application "Alfred 2" to search "{}"'""".format(arg.replace('"', '\\"')))
+
 
 match = re.match(r'^(.*?) @ (.*?) (\| (.*) )?([@|>]) (.*?)$', sys.argv[1])
 if match:
@@ -21,7 +31,9 @@ if match:
         print 'Definition copied to clipboard.'
     else:
         if operator == '@':
-            restore('dict {} @ {}'.format(word, command))
+            keyword = get_keyword()
+            if keyword:
+                restore('{} {} @ {}'.format(keyword, word, command))
         elif operator == '>':
             if command == 'say':
                 cndict.say(dictionary, word)
